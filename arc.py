0fe5ad78a0051f73caa2a7da6dd49e8d7129b630
Fitzgibbon <ibbolia@gmail.com>
2016-11-29 10:11:41 -0500

    Commands for help, src, dest implemented. Comments updated to describe functions

--- arc.py
+++ arc.py
@@ -6,17 +6,27 @@ import zipfile	#File compression
 #import getopt	#Argument handling
 import argparse	#Argument Handling
 
-def usage():
-	print("python arc.py src=sourcePath dst=destinationPath")
+#def usage():
+#	print("python arc.py src=sourcePath dst=destinationPath")
 
-def subZip(zf, sourceDir):									#Attempt to add all subfiles 
+def subZip(zf, sourceDir):
+	'''Attempt to add all files and directories in a folder to a given zip file.
+	Keyword Arguments:
+	zf -- Zip file to be written to.
+	sourceDir -- String representation of the directory's contents to be added
+	'''
 	allLocalFiles = os.listdir(sourceDir)					#Get all local files
 	for f in allLocalFiles:									#Loop through file names
 		zf.write(sourceDir + "\\" + f)						#Add a file to archive
 		if os.path.isdir(sourceDir+ "\\" + f):				#If file added is a directory
 			subZip(zf, sourceDir + "\\" + f)				#Recursively add subdirectory files
 
-def zipDirectory(sourceDir, targetDir):						#Begin archiving of a single directory
+def zipDirectory(sourceDir, targetDir):
+	'''Begin archiving a given directroy
+	Keyword Arguments:
+	sourceDir -- String representation of the directory to be archived
+	targetDir -- String representation of the location to save the given directory
+	'''
 	sourceStr = targetDir + "\\" + sourceDir + ".zip"		#String representation of archive location
 	print("Archiving "+ sourceDir + " to " + sourceStr)
 	zf = zipfile.ZipFile( sourceStr, 'w')					#Create Zip file
@@ -24,6 +34,11 @@ def zipDirectory(sourceDir, targetDir):						#Begin archiving of a single direct
 	zf.close()												#Close directory file
 
 def buildTo(sourceList, targetDir = None):
+	'''Confirms desired destination directory is available and initiates archiving on sourceList
+	Keyword Arguments:
+	sourceList -- List of all directories to be operated on
+	targetDir -- Destination to save all archive files. If None, save in cwd
+	'''
 	if(targetDir is None or not os.path.isdir(targetDir)):	#Expected target directory is inaccessible or undefined
 		print("Target directory " + targetDir + " inaccessible")
 		targetDir = ""										#Build locally
@@ -31,6 +46,10 @@ def buildTo(sourceList, targetDir = None):
 		zipDirectory(f, targetDir)							#Create zip archive of each directory (uncompressed)
 
 def getSourceDirs(targetRoot = None):
+	'''Get all directories in a given destination. 
+	Keyword Arguments:
+	targetRoot -- String representation of target folder. If none, works on cwd
+	''' 
 	targetDirs = []											#Initialize list
 	if(targetRoot != None):									#User specifies files are located elsewhere
 		print("Attempting CD to " + targetRoot)				#Currently can do nothing
@@ -47,31 +66,18 @@ def main(argv):												#Run version check and execute script if valid
 		usage()
 		return -1											#RETURNS: -1
 	
-	dstDir = None
-	srcDir = None
+	#dstDir = None
+	#srcDir = None
 
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description="Create ZIP archives of all directories in a given folder")
 	parser.add_argument("dest", help="Destination of generated archives")
-	parser.add_argument("-c", "--compress", help="Compress generated archives[NOT IMPLEMENTED]", action="store_true")
-	parser.parse_args()
-	'''
-	try:
-		opts, args = getopt.getopt(argv, "hs:d:", ["help","src=" "dst="])
-	except getopt.GetoptError:
-		usage()
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			usage()
-			sys.exit()
-		elif opt in ("-d", "--dst"):
-			dstDir = arg
-		elif opt in ("-s", "--src"):
-			srcDir = arg
-'''
-	#lst = getSourceDirs(srcDir)
-	#buildTo(lst, dstDir)
-	#print("Mass archiving complete.")
+	parser.add_argument("--src", "-s", help="Location of folders to be archived (defaults to current working directory).")
+	#parser.add_argument("-c", "--compress", help="Compress generated archives[NOT IMPLEMENTED]", action="store_true")
+	args = parser.parse_args()
+
+	lst = getSourceDirs(args.src)							#Get list of directories
+	buildTo(lst, args.dest)									#Archive list to destination
+	print("Mass archiving complete.")
 	return 0												#RETURNS: 0
 
 if __name__ == "__main__":
